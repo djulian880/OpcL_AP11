3de3a74170ca0d870eaad4d128d1ee9082523bf8
Julien GUTTER <julien.gutter@gmail.com>
2026-04-26 22:07:35 +0200

    Initial commit

---
+++ micro-bed-availbility/src/main/java/com/openclassrooms/micro_bed_availbility/MicroBedAvailbilityApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.micro_bed_availbility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBedAvailbilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroBedAvailbilityApplication.class, args);
+	}
+
+}
