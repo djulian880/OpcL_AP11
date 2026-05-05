bc3330aa0b5328ff7a650946f44e2791b6f53955
Julien GUTTER <julien.gutter@gmail.com>
2026-05-05 22:45:43 +0200

    Maj

---
+++ micro-bed-availbility/src/main/java/com/openclassrooms/micro_bed_availbility/MicroBedAvailbilityApplication.java
@@ -0,0 +1,18 @@
+package com.openclassrooms.micro_bed_availbility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBedAvailbilityApplication {
+
+
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MicroBedAvailbilityApplication.class, args);
+
+	}
+
+
+}
