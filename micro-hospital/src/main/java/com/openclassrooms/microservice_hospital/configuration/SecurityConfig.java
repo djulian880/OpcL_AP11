b25c9d9c3231bc42bf9d2f64c2f55ede51c16d2f
Julien GUTTER <julien.gutter@gmail.com>
2026-05-12 10:25:54 +0300

    Modification paramètre pour endpoints

--- micro-hospital/src/main/java/com/openclassrooms/microservice_hospital/configuration/SecurityConfig.java
+++ micro-hospital/src/main/java/com/openclassrooms/microservice_hospital/configuration/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/specialties/**").permitAll()
+                        .requestMatchers("/specialities/**").permitAll()
                         .anyRequest().authenticated()
                 );
         return http.build();
