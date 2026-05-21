7a8cf84dfa87ddc0a016bcbc422042649af94b43
Julien GUTTER <julien.gutter@gmail.com>
2026-05-21 20:27:42 +0300

    Maj test

--- micro-bed-availbility/src/test/java/com/openclassrooms/micro_bed_availability/MicroBedAvailabilityApplicationTests.java
+++ micro-bed-availbility/src/test/java/com/openclassrooms/micro_bed_availability/MicroBedAvailabilityApplicationTests.java
@@ -1,12 +1,13 @@
 package com.openclassrooms.micro_bed_availability;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MicroBedAvailabilityApplicationTests {
-/*
+
 	@Test
 	void contextLoads() {
-	}*/
+	}
 
 }
