906eb930525980e2fc38dfd4bc732ef6859a1e5c
Julien GUTTER <julien.gutter@gmail.com>
2026-05-21 20:51:33 +0300

    Maj

--- micro-bed-availbility/src/test/java/com/openclassrooms/micro_bed_availability/infra/AppointmentFetcherTest.java
+++ micro-bed-availbility/src/test/java/com/openclassrooms/micro_bed_availability/infra/AppointmentFetcherTest.java
@@ -1,7 +1,6 @@
-package com.openclassrooms.micro_bed_availability;
+package com.openclassrooms.micro_bed_availability.infra;
 
 import com.openclassrooms.micro_bed_availability.domain.fetch.Appointment;
-import com.openclassrooms.micro_bed_availability.infra.AppointmentFetcher;
 import com.openclassrooms.micro_bed_availability.infra.proxies.MicroGatewayProxy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
